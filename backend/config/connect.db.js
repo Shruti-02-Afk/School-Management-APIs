0827a7a5fcfe5fbd10c0963ae0d61b739d87c7cd
Shruti Shaw <shrutishaw750@gmail.com>
2025-03-18 23:46:12 +0530

    Updated database connection to use Railway MySQL

--- backend/config/connect.db.js
+++ backend/config/connect.db.js
@@ -3,11 +3,12 @@ dotenv.config();
 import mysql from "mysql2";
 
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
 });
 
 db.connect((err)=>{
@@ -16,7 +17,39 @@ db.connect((err)=>{
     } else {
         console.log(`Database connected successfully`);
     }
+});*/
+
+
+const MYSQL_URL = process.env.MYSQL_URL;
+
+// Parse DATABASE_URL
+const regex = /mysql:\/\/(.*):(.*)@(.*):(\d+)\/(.*)/;
+const match = MYSQL_URL.match(regex);
+
+if (!match) {
+    throw new Error("Invalid MYSQL_URL format");
+}
+
+const [ , user, password, host, port, database ] = match;
+
+const db = mysql.createConnection({
+    host,
+    user,
+    password,
+    database,
+    port: Number(port),
 });
+
+db.connect((err) => {
+    if(err){
+        console.log(`Database connection failed : ${err}`);
+    } else {
+        console.log(`Database connected successfully`);
+    }
+});
+
+
+
 
 export default db ;
 
